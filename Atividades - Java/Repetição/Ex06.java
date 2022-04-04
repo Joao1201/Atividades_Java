2a9e15d5e7816080916a18141a3fe863d7d588ad
João Vitor Felix de Andrade <joao.felixandrade1201@gmail.com>
2022-04-04 16:30:52 -0300

    Corrigindo Ex06

--- Atividades - Java/Repetição/Ex06.java
+++ Atividades - Java/Repetição/Ex06.java
@@ -9,13 +9,13 @@ final imprimir a média dos números múltiplos de 3. Para sair digitar
 	public static void main(String[] args) {
 		
 		Scanner entrada = new Scanner(System.in);
-		int i=1, num=0, a=-1, media=0;
+		int i=1, num=0, a=0, media=0;
 		
 		do {
 			System.out.print("Digite um número inteiro: ");
 			i = entrada.nextInt();
 			
-			if(i%3==0) {
+			if(i%3==0 && i!=0) {
 				num = num+i;
 				a++;
 			}
