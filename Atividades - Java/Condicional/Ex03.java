ea93ec5e90d89c7c420cdec4747d53e9068a3a93
João Vitor Felix de Andrade <joao.felixandrade1201@gmail.com>
2022-04-04 08:05:52 -0300

    Atualizando Ex03

--- Atividades - Java/Condicional/Ex03.java
+++ Atividades - Java/Condicional/Ex03.java
@@ -13,17 +13,22 @@ categoria ela se encontra:
 		Scanner entrada = new Scanner(System.in);
 		int idade;
 		
+		System.out.println("Digite seu nome: ");
+		String nome = entrada.nextLine();
 		System.out.println("Digite sua idade: ");
 		idade = entrada.nextInt();
 		
 		System.out.println();
 		if(idade>=10 && idade<=14) {
+			System.out.println("Olá "+nome+"!");
 			System.out.println("Sua categoria é: Infantil");
 		}
 		else if(idade>=15 && idade<=17) {
+			System.out.println("Olá "+nome+"!");
 			System.out.println("Sua categoria é: Juvenil");
 		}
 		else if(idade>=18 && idade<=25) {
+			System.out.println("Olá "+nome+"!");
 			System.out.println("Sua categoria é: Adulto");
 		}
 		else {
